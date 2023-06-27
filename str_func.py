73b3476c8f1c2362ff4a8a4336eab7bf0f1248af
egor <shievanov@bk.ru>
2023-06-27 22:51:19 +0300

    add func first_word and dosctring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def big_word(text):
     return text.upper()
+
+def first_word(text):
+    """
+    Получает строку и увеличивает первую букву
+    :param text:
+    :return:слова с заглавной буквой
+    """
+    return text.title()
+
