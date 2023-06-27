81fd01a0baf8c77edd0c969b2b9ff5db88bb3cd2
Plutarxi99 <132927381+Plutarxi99@users.noreply.github.com>
2023-06-27 22:56:03 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -5,10 +5,5 @@ def big_word(text):
     return text.upper()
 
 def first_word(text):
-    """
-    Получает строку и увеличивает первую букву
-    :param text:
-    :return:слова с заглавной буквой
-    """
     return text.title()
 
